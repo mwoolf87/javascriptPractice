949b23952a1233e569afeedc8676cb83d2d7b693
mwoolf87 <michael.j.woolf@gmail.com>
2021-05-20 21:25:36 -0400

    refactored the message portion

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -17,16 +17,20 @@ let score = 20;
 
 let highScore = 0;
 
+let displayMessage = message => {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', () => {
   let guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   // When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = '🚨 No Number!';
+    displayMessage('🚨 No Number!');
     // When player wins
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    displayMessage('🎉 Correct Number!');
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
@@ -35,20 +39,16 @@ document.querySelector('.check').addEventListener('click', () => {
       highScore = score;
       document.querySelector('.highscore').textContent = highScore;
     }
-    // When guess is too high
-  } else if (guess > secretNumber) {
+    //When guess it wrong
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Too High!';
+      displayMessage(guess > secretNumber ? '📈 Too High!' : '📉 Too Low!');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.score').textContent = 'You lost the game!';
+      displayMessage('You lost the game!');
+      document.querySelector('.score').textContent = 0;
     }
-    //When guess is too low
-  } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '📉 Too Low!';
-    score--;
-    document.querySelector('.score').textContent = score;
   }
 });
 //// --------- Coding Challenge 1 --------- /////
