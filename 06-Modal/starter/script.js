5cb201aa2bbc8e8cc857897eb68e945a1db96ced
mwoolf87 <michael.j.woolf@gmail.com>
2021-05-23 11:23:35 -0400

    added an event listener for the escape key to close modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -4,21 +4,27 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
 
 const openModal = () => {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
-
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
-
 const closeModal = () => {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
 btnCloseModal.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
+
+//in the below example the function is defined and the event object is passed in as an argument. 'Escape' is the event key being triggered and if the event is equal to 'Escape' then the modal will close using the function above
+document.addEventListener('keydown', e => {
+  //if the e.key is equal to escape and if the modal does NOT contain the class 'hidden' then, close the modal/
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
